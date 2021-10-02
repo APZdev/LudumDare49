a38434eb669a79c8a71e8a9829b4e20f487ee104
Adam <44951876+APZdev@users.noreply.github.com>
2021-10-02 13:20:45 +0200

    Boat commit test

--- Assets/ReadMe.cs
+++
@@ -1,4 +0,0 @@
-
-
-    //Les images dite SVG (image vectoriel) sont des images qui ne sont jamais pixaliser
-    //Pour les importer dans la scene il faut soit les drag & drop, soit modifier le sprite renderer (le sprite en lui même, et le shader en Unlit_Vectorial Shader)

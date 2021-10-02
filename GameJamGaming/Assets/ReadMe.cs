8c0daed16f8be054efa215f3d9e7f5652d2d1baf
Nathan-EVRARD <91688951+Nathan-EVRARD@users.noreply.github.com>
2021-10-02 10:06:38 +0200

    Implémentation du Package VectorGraphic
    Lire le ReadMe créé dans le projet

---
+++ GameJamGaming/Assets/ReadMe.cs
@@ -0,0 +1,4 @@
+
+
+    //Les images dite SVG (image vectoriel) sont des images qui ne sont jamais pixaliser
+    //Pour les importer dans la scene il faut soit les drag & drop, soit modifier le sprite renderer (le sprite en lui même, et le shader en Unlit_Vectorial Shader)
